d54f2ae88b78b935bd912f13fc3acf43cbb2a6b7
Javier Diaz <hey@coderdiaz.me>
2021-06-13 00:28:54 -0500

    feat: added custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@700&family=Inter:wght@400;500&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
