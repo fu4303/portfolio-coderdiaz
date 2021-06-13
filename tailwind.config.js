601a15e83b95efc8b937737c97283f5c19a2c468
Javier Diaz <hey@coderdiaz.me>
2021-06-13 00:30:23 -0500

    feat: added custom fonts to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [
-    './components/**/*.js',
-    './pages/**/*.js',
+    './components/**/*.tsx',
+    './pages/**/*.tsx',
+    './layouts/**/.tsx'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      'display': ['DM Sans', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
