3cebcef6c1fb9c3a0b5662f4cad6a3fd838948b2
Javier Diaz <hey@coderdiaz.me>
2021-06-14 00:11:38 -0500

    feat: added custom fonts to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@700&family=Inter:wght@400;500&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />
   </>;
